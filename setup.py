6c2d73bdfd1c97b7182ffafa9018788918a5eb78
narekboghozian <66885970+narekboghozian@users.noreply.github.com>
2021-10-26 20:07:51 -0700

    .

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='sqlite3_wrapper',
-    packages=find_packages(include=['sqlite3_wrapper']),
+    packages=find_packages(),
     version='0.1.0',
     description='Wrapper for Sqlite3 in Python',
     author='Narek Boghozian',
